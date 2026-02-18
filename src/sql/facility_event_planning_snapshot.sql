d09af1695b2a338039a9af39399ec488a88f9f03
YasuhiroTakemura <yasuhirotakemura0614@gmail.com>
2026-02-18 10:06:32 +0900

    fix: クエリ修正

--- src/sql/facility_event_planning_snapshot.sql
+++ src/sql/facility_event_planning_snapshot.sql
@@ -8,30 +8,52 @@ WITH stats_summary AS (
     FROM `digital-well-456700-i9.docomo_eventActual.facility_daily_actual` AS f_d_a
     GROUP BY
         f_d_a.facility_name
+), base AS (
+    SELECT
+        e_d_b.facility_name,
+        e_d_b.po_level,
+        e_d_b.branch_office,
+        CONCAT(CAST(e_d_b.month AS STRING), '月') AS month,
+        '第' || CAST(e_d_b.week_number_monthly AS STRING) || '週' AS week_number_monthly,
+        e_d_b.event_type AS `日付種別`,
+        f_e_d_m_a.max_actual AS daily_actual,
+        s_s.latest_actual,
+        e_d_b.decile_rank,
+        e_d_b.p25, -- 実績下位25%の実績値
+        e_d_b.p50, -- デシル区分の中央値
+        e_d_b.p60, -- 実績上位40%の実績値
+        e_d_b.p70, -- 実績上位30%の実績値
+        e_d_b.p75, -- 実績上位25%の実績値
+        e_d_b.p90, -- 実績上位10%の実績値
+        e_d_b.max_performance, -- デシル区分の最大の実績値
+        CASE
+            WHEN f_e_d_m_a.max_actual IS NULL OR f_e_d_m_a.max_actual < e_d_b.p50 THEN e_d_b.p50
+            WHEN f_e_d_m_a.max_actual >= e_d_b.max_performance THEN e_d_b.max_performance
+            WHEN f_e_d_m_a.max_actual < e_d_b.p50 THEN e_d_b.p50
+            WHEN f_e_d_m_a.max_actual < e_d_b.p60 THEN e_d_b.p60
+            WHEN f_e_d_m_a.max_actual < e_d_b.p70 THEN e_d_b.p70
+            WHEN f_e_d_m_a.max_actual < e_d_b.p75 THEN e_d_b.p75
+            WHEN f_e_d_m_a.max_actual < e_d_b.p90 THEN e_d_b.p90
+            ELSE e_d_b.max_performance
+        END AS standard_target -- 標準目標
+    FROM `digital-well-456700-i9.docomo_eventActual.event_decile_benchmark` AS e_d_b
+    LEFT JOIN `digital-well-456700-i9.docomo_eventActual.facility_event_decile_max_actual` AS f_e_d_m_a
+        ON e_d_b.facility_name = f_e_d_m_a.facility_name
+        AND e_d_b.month = f_e_d_m_a.month
+        AND e_d_b.week_number_monthly = f_e_d_m_a.week_number_monthly
+        AND e_d_b.event_type = f_e_d_m_a.event_type
+        AND e_d_b.decile_rank = f_e_d_m_a.decile_rank
+    LEFT JOIN stats_summary AS s_s
+        ON e_d_b.facility_name = s_s.facility_name
 )
 SELECT
-    e_d_b.facility_name AS `施設名`,
-    e_d_b.po_level AS `POレベル`,
-    e_d_b.branch_office AS `支社`,
-    CONCAT(CAST(e_d_b.month AS STRING), '月') AS `実績月`,
-    '第' || CAST(e_d_b.week_number_monthly AS STRING) || '週' AS `週番号_月`,
-    e_d_b.event_type AS `日付種別`,
-    f_e_d_m_a.max_actual AS `日付実績`,
-    s_s.latest_actual AS `直近実績`,
-    e_d_b.decile_rank AS `デシル区分`,
-    e_d_b.p25 AS `25%`, -- 実績下位25%の実績値
-    e_d_b.p50 AS `50%`, -- デシル区分の中央値
-    e_d_b.p60 AS `60%`, -- 実績上位40%の実績値
-    e_d_b.p70 AS `70%`, -- 実績上位30%の実績値
-    e_d_b.p75 AS `75%`, -- 実績上位25%の実績値
-    e_d_b.p90 AS `90%`, -- 実績上位10%の実績値
-    e_d_b.max_performance AS `MAX`, -- デシル区分の最大の実績値
-FROM `digital-well-456700-i9.docomo_eventActual.event_decile_benchmark` AS e_d_b
-LEFT JOIN `digital-well-456700-i9.docomo_eventActual.facility_event_decile_max_actual` AS f_e_d_m_a
-    ON e_d_b.facility_name = f_e_d_m_a.facility_name
-    AND e_d_b.month = f_e_d_m_a.month
-    AND e_d_b.week_number_monthly = f_e_d_m_a.week_number_monthly
-    AND e_d_b.event_type = f_e_d_m_a.event_type
-    AND e_d_b.decile_rank = f_e_d_m_a.decile_rank
-LEFT JOIN stats_summary AS s_s
-    ON e_d_b.facility_name = s_s.facility_name;
+    *,
+    CASE
+        WHEN standard_target >= max_performance THEN max_performance
+        WHEN standard_target < p60 THEN p60
+        WHEN standard_target < p70 THEN p70
+        WHEN standard_target < p75 THEN p75
+        WHEN standard_target < p90 THEN p90
+        ELSE max_performance
+    END AS challenge_target
+FROM base;
--- src/sql/facility_special_event_planning_summary.sql
+++ src/sql/facility_special_event_planning_summary.sql
@@ -8,34 +8,60 @@ WITH stats_summary AS (
     FROM `digital-well-456700-i9.docomo_eventActual.facility_daily_actual` AS f_d_a
     GROUP BY
         f_d_a.facility_name
+), base AS (
+    SELECT
+        e_d_b.facility_name,
+        e_d_b.po_level,
+        e_d_b.branch_office,
+        e_d_b.event_type,
+        ROUND(AVG(f_e_d_m_a.max_actual)) AS daily_actual,
+        ROUND(AVG(s_s.latest_actual)) AS latest_actual,
+        ROUND(AVG(e_d_b.decile_rank)) AS decile_rank,
+        ROUND(AVG(e_d_b.p25)) AS p25, -- 実績下位25%の実績値
+        ROUND(AVG(e_d_b.p50)) AS p50, -- デシル区分の中央値
+        ROUND(AVG(e_d_b.p60)) AS p60, -- 実績上位40%の実績値
+        ROUND(AVG(e_d_b.p70)) AS p70, -- 実績上位30%の実績値
+        ROUND(AVG(e_d_b.p75)) AS p75, -- 実績上位25%の実績値
+        ROUND(AVG(e_d_b.p90)) AS p90, -- 実績上位10%の実績値
+        ROUND(AVG(e_d_b.max_performance)) AS max_performance, -- デシル区分の最大の実績値
+    FROM `digital-well-456700-i9.docomo_eventActual.event_decile_benchmark` AS e_d_b
+    LEFT JOIN `digital-well-456700-i9.docomo_eventActual.facility_event_decile_max_actual` AS f_e_d_m_a
+        ON e_d_b.facility_name = f_e_d_m_a.facility_name
+        AND e_d_b.month = f_e_d_m_a.month
+        AND e_d_b.week_number_monthly = f_e_d_m_a.week_number_monthly
+        AND e_d_b.event_type = f_e_d_m_a.event_type
+        AND e_d_b.decile_rank = f_e_d_m_a.decile_rank
+    LEFT JOIN stats_summary AS s_s
+        ON e_d_b.facility_name = s_s.facility_name
+    WHERE e_d_b.event_type IN ('正月', '年末', 'GW', 'お盆', 'ブラックフライデー')
+    GROUP BY
+        e_d_b.facility_name,
+        e_d_b.po_level,
+        e_d_b.branch_office,
+        e_d_b.event_type
+), challenge_standard_summary AS (
+    SELECT
+        *,
+        CASE
+            WHEN daily_actual IS NULL OR daily_actual < p50 THEN p50
+            WHEN daily_actual >= max_performance THEN max_performance
+            WHEN daily_actual < p50 THEN p50
+            WHEN daily_actual < p60 THEN p60
+            WHEN daily_actual < p70 THEN p70
+            WHEN daily_actual < p75 THEN p75
+            WHEN daily_actual < p90 THEN p90
+            ELSE max_performance
+        END AS standard_target -- 標準目標
+    FROM base
 )
 SELECT
-    e_d_b.facility_name AS `施設名`,
-    e_d_b.po_level AS `POレベル`,
-    e_d_b.branch_office AS `支社`,
-    e_d_b.event_type AS `日付種別`,
-    ROUND(AVG(f_e_d_m_a.max_actual)) AS `日付実績`,
-    ROUND(AVG(s_s.latest_actual)) AS `直近実績`,
-    ROUND(AVG(e_d_b.decile_rank)) AS `デシル区分`,
-    ROUND(AVG(e_d_b.p25)) AS `25%`, -- 実績下位25%の実績値
-    ROUND(AVG(e_d_b.p50)) AS `50%`, -- デシル区分の中央値
-    ROUND(AVG(e_d_b.p60)) AS `60%`, -- 実績上位40%の実績値
-    ROUND(AVG(e_d_b.p70)) AS `70%`, -- 実績上位30%の実績値
-    ROUND(AVG(e_d_b.p75)) AS `75%`, -- 実績上位25%の実績値
-    ROUND(AVG(e_d_b.p90)) AS `90%`, -- 実績上位10%の実績値
-    ROUND(AVG(e_d_b.max_performance)) AS `MAX`, -- デシル区分の最大の実績値
-FROM `digital-well-456700-i9.docomo_eventActual.event_decile_benchmark` AS e_d_b
-LEFT JOIN `digital-well-456700-i9.docomo_eventActual.facility_event_decile_max_actual` AS f_e_d_m_a
-    ON e_d_b.facility_name = f_e_d_m_a.facility_name
-    AND e_d_b.month = f_e_d_m_a.month
-    AND e_d_b.week_number_monthly = f_e_d_m_a.week_number_monthly
-    AND e_d_b.event_type = f_e_d_m_a.event_type
-    AND e_d_b.decile_rank = f_e_d_m_a.decile_rank
-LEFT JOIN stats_summary AS s_s
-    ON e_d_b.facility_name = s_s.facility_name
-WHERE e_d_b.event_type IN ('正月', '年末', 'GW', 'お盆', 'ブラックフライデー')
-GROUP BY
-    e_d_b.facility_name,
-    e_d_b.po_level,
-    e_d_b.branch_office,
-    e_d_b.event_type
+    *,
+    CASE
+        WHEN standard_target >= max_performance THEN max_performance
+        WHEN standard_target < p60 THEN p60
+        WHEN standard_target < p70 THEN p70
+        WHEN standard_target < p75 THEN p75
+        WHEN standard_target < p90 THEN p90
+        ELSE max_performance
+    END AS challenge_target
+FROM challenge_standard_summary
